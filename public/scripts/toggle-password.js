6ac545289b9f7ffdf4ee342c136916ecdd5efb7f
Pedro Barão <phbarao@gmail.com>
2021-01-14 23:45:06 -0300

    toggle-password ok

--- public/scripts/toggle-password.js
+++ public/scripts/toggle-password.js
@@ -2,7 +2,6 @@ const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
 
 togglePassword.addEventListener("click", function (e) {
-  // toggle the type attribute
   const type =
     password.getAttribute("type") === "password" ? "text" : "password";
   password.setAttribute("type", type);
